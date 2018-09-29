695c99081ad41b50cfbcd630e4316be1e6e1a2cb
Christoph Reiter <reiter.christoph@gmail.com>
2018-09-29 19:25:27 +0200

    flake8

--- examples/gtk.py
+++ examples/gtk.py
@@ -1,1 +1,3 @@
 from gi.repository import Gtk
+
+Gtk
--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 import os
 from setuptools import setup
 
+
 def find_stubs(package):
     stubs = []
     for root, dirs, files in os.walk(package):
         for f in files:
             stubs.append(os.path.relpath(os.path.join(root, f), package))
     return {package: stubs}
+
 
 setup(
     name="PyGObject-stubs",
