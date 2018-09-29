249969d2fbc9ee49757b46beb2a557156693aac8
Christoph Reiter <reiter.christoph@gmail.com>
2018-09-29 19:20:01 +0200

    Add mypy tests

---
+++ examples/gtk.py
@@ -0,0 +1,1 @@
+from gi.repository import Gtk
--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
-from distutils.core import setup
+import os
+from setuptools import setup
+
+def find_stubs(package):
+    stubs = []
+    for root, dirs, files in os.walk(package):
+        for f in files:
+            stubs.append(os.path.relpath(os.path.join(root, f), package))
+    return {package: stubs}
 
 setup(
     name="PyGObject-stubs",
@@ -6,8 +14,7 @@ setup(
     author="Christoph Reiter",
     author_email="reiter.christoph@gmai.com",
     version="0.0.1",
-    package_data={
-        "gi-stubs": ["__init__.pyi"]},
+    package_data=find_stubs("gi-stubs"),
     packages=["gi-stubs"],
     install_requires=["PyGObject"],
 )
