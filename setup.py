2e077b8391ca1a1d7ce61eff9a7f53459eff3ebc
Mrinmoy Haloi <69071143+MrinmoyHaloi@users.noreply.github.com>
2022-01-11 18:06:06 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name="PyGObject-stubs",
+    description="Typing stubs for PyGobject",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url="https://github.com/pygobject/pygobject-stubs",
     author="Christoph Reiter",
     author_email="reiter.christoph@gmail.com",
