38378a2438ee996e4b55fb174e9d7fa44681cfe0
eggplants <w10776e8w@yahoo.co.jp>
2022-03-04 07:51:28 +0900

    Move packaging infomation from setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -1,24 +1,3 @@
 from setuptools import setup
 
-with open('README.md', 'r', encoding='utf-8') as fh:
-    long_description = fh.read()
-
-setup(
-    name="PyGObject-stubs",
-    description="Typing stubs for PyGobject",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pygobject/pygobject-stubs",
-    author="Christoph Reiter",
-    author_email="reiter.christoph@gmail.com",
-    version="0.0.7",
-    classifiers=[
-        'Intended Audience :: Developers',
-        "Programming Language :: Python",
-        "License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)",
-        "Operating System :: OS Independent",
-    ],
-    package_data={"gi-stubs": ["*.pyi", "*/*.pyi"]},
-    packages=["gi-stubs"],
-    install_requires=["PyGObject"],
-)
+setup()
