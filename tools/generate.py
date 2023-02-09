68b193cd47367ae4fca2203f2348fd52833fd1dc
Matteo Percivaldi <m.percivaldi@fastassembler.com>
2023-02-09 08:00:09 +0100

    isort

--- tools/generate.py
+++ tools/generate.py
@@ -7,8 +7,8 @@ from __future__ import annotations
 from typing import Any
 from typing import Callable
 from typing import Optional
-from typing import Type
 from typing import Tuple
+from typing import Type
 from typing import Union
 
 import argparse
